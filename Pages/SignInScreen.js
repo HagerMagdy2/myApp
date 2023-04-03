806279fadc12eeb9b071c1a70b8497c3a0d8ae69
arwaayman16 <aymanarwa05@gmail.com>
2023-04-03 22:08:14 +0200

    forgt bn added

--- Pages/SignInScreen.js
+++ Pages/SignInScreen.js
@@ -2,12 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, View ,TextInput,TouchableOpacity,Image} from 'react-native';
 import { Text } from 'react-native';
-<<<<<<< HEAD
 import {  createUserWithEmailAndPassword } from "firebase/auth";
 import auth from '../firebase';
 import { useState } from 'react';
 
-export default function SignInScreen({navigation}) {
+export default function RegisterScreen({navigation}) {
     const user = auth.currentUser;
     const[email , setEmail]=useState('');
     const[password , setPassword]=useState('');
@@ -16,7 +15,8 @@ export default function SignInScreen({navigation}) {
     .then((userCredential) => {
       navigation.navigate("WelcomeScreen");
       
-      const user = userCredential.user;
+      console.log("done")
+        const user = userCredential.user;
       // ...
     })
     .catch((error) => {
@@ -25,69 +25,15 @@ export default function SignInScreen({navigation}) {
       alert("Can not Creat this account may be you have an account with name " );
     });
     }
-=======
-import { signInWithEmailAndPassword, updateCurrentUser } from "firebase/auth";
-import auth from '../firebase';
-import { useState } from 'react';
-
-export default function SignIn({navigation}) {
-  const hundelSignIn =()=>{
-    signInWithEmailAndPassword(auth, email, password)
-.then((userCredential) => {
-  // Signed in 
-  console.log("done sign")
-
-  const user = userCredential.user;
-  // ...
-})
-.catch((error) => {
-  const errorCode = error.code;
-  const errorMessage = error.message;
-  console.log(errorMessage)
-});}
-
->>>>>>> 3dfe7753997face49284e7fd27d2ccb25b571ae1
   return (
     <View style={styles.container}>
        <Image style={styles.image} source={require("../assets/photo.png")} ></Image>
       {/* <image source={require("../assets/cover.png")}/> */}
-<<<<<<< HEAD
       <TouchableOpacity style={styles.button}
         onPress={handleSignUp}>
       <text style={styles.statmentButton}>SignIn</text>
-=======
-      
-
-      <StatusBar style="auto" /> 
-      
-      <View style={styles.inputView}>
-        <TextInput
-          style={styles.TextInput}
-          placeholder="Email.  "
-       //   value={email}
-          placeholderTextColor="#000"
-      //    onChangeText={setEmail}
-        /> 
-      </View> 
-      <View style={styles.inputView}>
-        <TextInput
-          style={styles.TextInput}
-          placeholder="Password "
-        //  value={password}
-
-          placeholderTextColor="#000"
-          secureTextEntry={true}
-         // onChangeText= {setPassword}
-         
-          
-        /> 
-        
-      <TouchableOpacity onPress={() => navigation.navigate("Forgot")}>
-        <Text style={styles.forgot_button}>Forgot Password?</Text> 
->>>>>>> 3dfe7753997face49284e7fd27d2ccb25b571ae1
       </TouchableOpacity>
       
-<<<<<<< HEAD
       <TextInput
         style={styles.inputE}
         onChangeText={setEmail}
@@ -105,26 +51,6 @@ export default function SignIn({navigation}) {
       />
      <StatusBar style="auto" />
       
-=======
-      <View > 
-    <TouchableOpacity style={styles.loginBtn}   onPress={hundelSignIn }>
-      <Text style={styles.Text}>SignIn  </Text> 
-    </TouchableOpacity> 
-<TouchableOpacity style={styles.loginBtn} onPress={()=>navigation.navigate("Home")}>
-      <Text style={styles.Text}>Home  </Text> 
-    </TouchableOpacity>
-   
-    {/* <TouchableOpacity style={styles.loginBtn} onPress={()=>navigation.navigat("LogIn")}>
-      <Text style={styles.Text}>LOGIN</Text> 
-    </TouchableOpacity>  */}
-    
-    <TouchableOpacity style={styles.loginBtn}   onPress={()=>navigation.navigate("SignUp")}>
-      <Text style={styles.Text}>SignUp          </Text> 
-    </TouchableOpacity> 
-   
-    
-    </View> 
->>>>>>> 3dfe7753997face49284e7fd27d2ccb25b571ae1
     </View>
   );
 }
