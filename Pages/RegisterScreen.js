6885c7576460500e349665e8772e4ac03e19fd79
HagerMagdy2 <hager9671@gmail.com>
2023-04-03 21:47:15 +0200

    Register

--- Pages/RegisterScreen.js
+++ Pages/RegisterScreen.js
@@ -2,24 +2,52 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, View ,TextInput,TouchableOpacity,Image} from 'react-native';
 import { Text } from 'react-native';
-
-import sora from "../assets/cover.png";
+import {  createUserWithEmailAndPassword } from "firebase/auth";
+import auth from '../firebase';
+import { useState } from 'react';
 
 export default function RegisterScreen({navigation}) {
-  
+    const user = auth.currentUser;
+    const[email , setEmail]=useState('');
+    const[password , setPassword]=useState('');
+    const handleSignUp = ()=>{
+      createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      navigation.navigate("WelcomeScreen");
+      
+      const user = userCredential.user;
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      alert("Can not Creat this account may be you have an account with name " );
+    });
+    }
   return (
     <View style={styles.container}>
        <Image style={styles.image} source={require("../assets/photo.png")} ></Image>
       {/* <image source={require("../assets/cover.png")}/> */}
-      <TouchableOpacity onPress={()=>navigation.navigate("SignIn")} >
-      <text >SIGN IN</text>
-      
+      <TouchableOpacity style={styles.button}
+        onPress={handleSignUp}>
+      <text style={styles.statmentButton}>Sign Up</text>
       </TouchableOpacity>
-      <TouchableOpacity  onPress={()=> navigation.navigate("Register") } >
-        <text >  REGISTER</text>
-       
-        </TouchableOpacity>
-
+      
+      <TextInput
+        style={styles.inputE}
+        onChangeText={setEmail}
+        value={email}
+        placeholder="Enter Your E-Mail"
+        keyboardType='email-address'
+      />
+<TextInput
+        style={styles.input}
+        onChangeText={setPassword}
+        value={password}
+        placeholder="Enter Your Password"
+        keyboardType='visible-password'
+        secureTextEntry
+      />
      <StatusBar style="auto" />
       
     </View>
@@ -39,4 +67,59 @@ const styles = StyleSheet.create({
         height: 1080 ,
         position: 'absolute',
       },
+      input: {
+        borderRadius:50,
+        height: 40,
+        width: 400,
+        margin: 12,
+        borderWidth: 0.5,
+        padding: 5,
+        position: 'absolute',
+        right: 50,
+        bottom: 220,
+      },
+      inputE: {
+        borderRadius:50,
+        height: 40,
+        width: 400,
+        margin: 12,
+        borderWidth: 0.5,
+        padding: 5,
+        position: 'absolute',
+        right: 50,
+        bottom: 300,
+      },
+      statmentButton: {
+        color: '#000',
+        fontFamily:'italic',
+        
+        fontWeight: 'bold',
+        fontSize: 25,
+        alignSelf: 'center',
+        paddingHorizontal: 8,
+        paddingVertical: 6,
+        borderRadius: 15,
+        marginBottom: 5,
+        minWidth: '50%',
+        textAlign: 'center',
+        position: 'relative',
+        bottom: 0,
+       // right: 50,
+  },
+  button: {
+    paddingHorizontal: 8,
+        paddingVertical: 6,
+        borderRadius: 50,
+        backgroundColor: '#713522',
+        alignSelf: 'auto',
+        //marginHorizontal: '1%',
+        marginBottom: 6,
+        minWidth: '30%',
+        textAlign: 'center',
+        position: 'absolute',
+        bottom: 100,
+        right: 205,
+        width: 45,
+        height: 50,
+  },
 });
