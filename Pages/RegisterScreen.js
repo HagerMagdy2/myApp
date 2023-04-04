79a8ca05b30e5397f6b4095a846047c5c62c3b19
maiatef17 <maiatef17@gmail.com>
2023-04-04 14:40:42 +0200

    start

--- Pages/RegisterScreen.js
+++ Pages/RegisterScreen.js
@@ -2,117 +2,125 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, View ,TextInput,TouchableOpacity,Image} from 'react-native';
 import { Text } from 'react-native';
-import { sendPasswordResetEmail } from "firebase/auth";
+import {  createUserWithEmailAndPassword } from "firebase/auth";
 import auth from '../firebase';
 import { useState } from 'react';
-export default function ForgetPass({navigation}){
-const[Email,setEmail]=useState('')
 
-const handleSignIn = () => {
-    
-      navigation.navigate("SignIn");
-}
-const handleForgotPassword= () => {
-sendPasswordResetEmail(auth, Email)
-  .then(() => {
-    console.log("password sent")
-    // Password reset email sent!
-    // ..
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    console.log("Email doesn't Exist")
-    // ..
-  });
-}
+export default function RegisterScreen({navigation}) {
+    const user = auth.currentUser;
+    const[email , setEmail]=useState('');
+    const[password , setPassword]=useState('');
+    const handleSignUp = ()=>{
+      createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      navigation.navigate("Welcome");
+      
+      const user = userCredential.user;
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // alert("Can not Creat this account may be you have an account with name " );
+      console.log(errorMessage);
+    });
+    }
   return (
     <View style={styles.container}>
-      <Image style={styles.image} source={require("../assets/Forget.jpg")} ></Image>
-      {/* <Text style={styles.title}>Enter your email below and we'll help you to reset your password</Text> */}
-      <TextInput style={styles.inputE}
+      <Image style={styles.image} source={require("../assets/Register.jpg")} ></Image>
+      {/* <image source={require("../assets/cover.png")}/> */}
+      <TouchableOpacity style={styles.button}
+        onPress={handleSignUp}>
+      <text style={styles.statmentButton}>Sign Up</text>
+      </TouchableOpacity>
+      
+      <TextInput
+        style={styles.inputE}
         onChangeText={setEmail}
-        value={Email}
+        value={email}
         placeholder="Enter Your E-Mail"
         keyboardType='email-address'
       />
-  <TouchableOpacity style={styles.Sendbutton} onPress={handleForgotPassword}>
-            <Text style={styles.statmentButton}>Send Link</Text>          
-          </TouchableOpacity>
-          {/* <TouchableOpacity style={styles.button} onPress={handleSignIn}>
-            <Text style={styles.buttonText}>Back to Sign in</Text>
-                  
-          </TouchableOpacity> */}
-          
+<TextInput
+        style={styles.input}
+        onChangeText={setPassword}
+        value={password}
+        placeholder="Enter Your Password"
+        keyboardType='visible-password'
+        secureTextEntry
+      />
+    <StatusBar style="auto" />
+      
     </View>
   );
 }
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    image: {
+        //flex: 1,
+        //justifyContent: 'center',
+        width:600,
+        height: 1080 ,
+        position: 'absolute',
+      },
+      input: {
+        borderRadius:50,
+        height: 40,
+        width: 400,
+        margin: 12,
+        borderWidth: 0.5,
+        padding: 5,
+        position: 'absolute',
+        right: 50,
+        bottom: 220,
+      },
+      inputE: {
+        borderRadius:50,
+        height: 40,
+        width: 400,
+        margin: 12,
+        borderWidth: 0.5,
+        padding: 5,
+        position: 'absolute',
+        right: 50,
+        bottom: 300,
+      },
+      statmentButton: {
+        color: '#FFFCF8',
+        fontFamily:'italic',
+        
+        fontWeight: 'bold',
+        fontSize: 25,
+        alignSelf: 'center',
+        paddingHorizontal: 8,
+        paddingVertical: 6,
+        borderRadius: 15,
+        marginBottom: 5,
+        minWidth: '50%',
+        textAlign: 'center',
+        position: 'relative',
+        bottom: 0,
+       // right: 50,
   },
-  image: {
-      //flex: 1,
-      //justifyContent: 'center',
-      width:600,
-      height: 1080 ,
-      position: 'absolute',
-    },
-    input: {
-      borderRadius:50,
-      height: 40,
-      width: 400,
-      margin: 12,
-      borderWidth: 0.5,
-      padding: 5,
-      position: 'absolute',
-      right: 50,
-      bottom: 220,
-    },
-    inputE: {
-      borderRadius:50,
-      height: 40,
-      width: 400,
-      margin: 12,
-      borderWidth: 1,
-      padding: 5,
-      position: 'absolute',
-      right: 120,
-      bottom: 200,
-    },
-    statmentButton: {
-      color: '#FFFCF8',
-      fontFamily:'italic',
-      fontSize:20,
-      
-      fontWeight: 'bold',
+  button: {
+    paddingHorizontal: 8,
+        paddingVertical: 6,
+        borderRadius: 50,
+        backgroundColor: '#713522',
+        alignSelf: 'auto',
+        //marginHorizontal: '1%',
+        marginBottom: 6,
+        minWidth: '30%',
+        textAlign: 'center',
+        position: 'absolute',
+        bottom: 100,
+        right: 205,
+        width: 45,
+        height: 50,
 },
-
-Sendbutton:{
-      
-  width: "50%",
-  borderRadius: 20,
-  height: 50,
-  alignItems: "center",
-  justifyContent: "center",
-  marginTop: 70,
-  backgroundColor: "#713522",
- // flexDirection: 'row',
-  paddingHorizontal: 100,
-  paddingVertical: 6,
-  borderRadius: 50,
-  marginBottom: 6,
-  minWidth: '30%',
-  textAlign: 'center',
-  position: 'absolute',
-top: 800,
-right:150,
-  width:350,
-
-
-},
-
 });
