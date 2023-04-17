10f147fb544c8144bc80b9dd729873e613c3e56c
Hager Magdy <120759962+HagerMagdy2@users.noreply.github.com>
2023-04-18 00:35:41 +0200

    l

--- Pages/RegisterScreen.js
+++ Pages/RegisterScreen.js
@@ -1,108 +1,240 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, View ,TextInput,TouchableOpacity,Image} from 'react-native';
-import { Text } from 'react-native';
-import {  createUserWithEmailAndPassword } from "firebase/auth";
-import auth from '../firebase';
-import { useState } from 'react';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import { Text } from "react-native";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth, db } from "../firebase";
+import { useState } from "react";
+import { doc, setDoc } from "firebase/firestore";
+//const user = auth.currentUser;
+export default function RegisterScreen({ navigation }) {
+  
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [nameError, setNameError] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
 
+  const handleNameChange = (text) => {
+    setName(text);
+    setNameError('');
+  };
 
-export default function RegisterScreen({navigation}) {
-    const user = auth.currentUser;
-    const[email , setEmail]=useState('');
-    const[password , setPassword]=useState('');
-    const handleSignUp = ()=>{
-      createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      navigation.navigate("Welcome");
-      
-      const user = userCredential.user;
-      // ...
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // alert("Can not Creat this account may be you have an account with name " );
-      //console.log(errorMessage);
-    });
+  const handleEmailChange = (text) => {
+    setEmail(text);
+    setEmailError('');
+  };
+
+  const handlePasswordChange = (text) => {
+    setPassword(text);
+    setPasswordError('');
+  };
+
+  const handleSubmit = () => {
+    // Perform validation
+    let valid = true;
+
+    if (name.trim()==="") {
+      setNameError("Please enter your name");
+      valid = false;
     }
-    
 
+    if (email.trim()==="") {
+      setEmailError("Please enter your email");
+      valid = false;
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
+      setEmailError("Please enter a valid email address");
+      valid = false;
+    }
 
+    if (password.trim()==="") {
+      setPasswordError("Please enter your password");
+      valid = false;
+    } else if (password.length < 6) {
+      setPasswordError("Password must be at least 6 characters");
+      valid = false;
+    }
+
+    if (valid) {
+      // Submit form
+      handleSignUp();
+    }
+  };
+
+  const handleSignUp = () => {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        navigation.navigate("Welcome");
+        console.log("Done");
+
+        const user = userCredential.user;
+        AddUserToDatabase();
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // alert("Can not Creat this account may be you have an account with name " );
+        console.log(errorMessage);
+      });
+  };
+  const AddUserToDatabase = async () => {
+    await setDoc(doc(db, "users", auth.currentUser.uid), {
+      email: email,
+      name: name,
+    });
+  };
   return (
     <View style={styles.container}>
-      <Image style={styles.image} source={require("../assets/Register.jpg")} ></Image>
+      <Image
+        style={styles.image}
+        source={require("../assets/Register.jpg")}
+      ></Image>
       {/* <image source={require("../assets/cover.png")}/> */}
+
+      <TouchableOpacity style={styles.button} onPress={handleSignUp}>
+        <text style={styles.statmentButton}>Sign Up</text>
+        </TouchableOpacity>
+
+   
       <TouchableOpacity style={styles.button}
         onPress={handleSignUp}>
       <text style={styles.statmentButton}>Sign Up</text>
+
       </TouchableOpacity>
-      <View>
-      <TouchableOpacity style={styles.HomeBtn} onPress={()=>navigation.navigate("HomeScreen")}>
+     
+  <View>  
+     <TouchableOpacity style={styles.HomeBtn} onPress={()=>navigation.navigate("HomeScreen")}>
       <Text style={styles.statmentButton}>  Home  </Text> 
     </TouchableOpacity>
-      </View>
-     
-      
+    </View>
+
+      <TextInput
+        style={styles.inputN}
+        onChangeText={handleNameChange}
+        value={name}
+        placeholder="Enter Your Name"
+        keyboardType="email-address"
+      />
+       {nameError ? <Text style={styles.error}>{nameError}</Text> : null}
       <TextInput
         style={styles.inputE}
-        onChangeText={setEmail}
+        onChangeText={handleEmailChange}
         value={email}
         placeholder="Enter Your E-Mail"
-        keyboardType='email-address'
+        keyboardType="email-address"
       />
-<TextInput
+      {emailError ? <Text style={styles.error}>{emailError}</Text> : null}
+      <TextInput
         style={styles.input}
-        onChangeText={setPassword}
+        onChangeText={handlePasswordChange}
         value={password}
         placeholder="Enter Your Password"
-        keyboardType='visible-password'
+        keyboardType="visible-password"
         secureTextEntry
       />
-     
-     
-    <StatusBar style="auto" />
 
-    
-      
+      {passwordError ? (
+        <Text style={styles.error}>{passwordError}</Text>
+      ) : null}
+      <StatusBar style="auto" />
+
+
     </View>
   );
 }
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    image: {
-        //flex: 1,
-        //justifyContent: 'center',
-        width:600,
-        height: 1080 ,
-        position: 'absolute',
-      },
-      HomeBtn: {
-        paddingHorizontal: 8,
-            paddingVertical: 6,
-            borderRadius: 50,
-            fontFamily:'italic',
-            backgroundColor: '#713522',
-            alignSelf: 'auto',
-            //marginHorizontal: '1%',
-            marginBottom: 6,
-            fontWeight: 'bold',
-            minWidth: '30%',
-            textAlign: 'center',
-            position: 'absolute',
-            bottom: 100,
-            right: -90,
-            width: 180,
-            bottom:-450,
-            height:50,
-      },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    //flex: 1,
+    //justifyContent: 'center',
+    width: 600,
+    height: 1080,
+    position: "absolute",
+  },
+  input: {
+    borderRadius: 5,
+    height: 40,
+    width: 400,
+
+    margin: 12,
+    borderWidth: 0.5,
+    padding: 5,
+    position: "absolute",
+    right: 50,
+    bottom: 220,
+  },
+  inputN: {
+    borderRadius: 5,
+    height: 40,
+    width: 400,
+
+    margin: 12,
+    borderWidth: 0.5,
+    padding: 5,
+    position: "absolute",
+    right: 50,
+    bottom: 380,
+  },
+  inputE: {
+    borderRadius:5,
+    height: 40,
+    width: 400,
+    margin: 12,
+    borderWidth: 0.5,
+    padding: 5,
+    position: "absolute",
+    right: 50,
+    bottom: 300,
+  },
+  statmentButton: {
+    color: "#FFFCF8",
+    fontFamily: "italic",
+    fontWeight: "bold",
+    fontSize: 25,
+    alignSelf: "center",
+    paddingHorizontal: 8,
+    paddingVertical: 6,
+    borderRadius: 15,
+    marginBottom: 5,
+    minWidth: "50%",
+    textAlign: "center",
+    position: "relative",
+    bottom: 0,
+    // right: 50,
+  },
+  button: {
+    paddingHorizontal: 8,
+    paddingVertical: 6,
+    borderRadius: 50,
+    backgroundColor: "#713522",
+    alignSelf: "auto",
+    //marginHorizontal: '1%',
+    marginBottom: 6,
+    minWidth: "30%",
+    textAlign: "center",
+    position: "absolute",
+    bottom: 100,
+    right: 205,
+    width: 45,
+    height: 50,
+  },
+
+
       input: {
-        borderRadius:10,
+        borderRadius:7,
         height: 40,
         width: 400,
         margin: 12,
@@ -112,8 +244,19 @@ const styles = StyleSheet.create({
         right: 50,
         bottom: 220,
       },
-      inputE: {
-        borderRadius:10,
+       inputN: {
+        borderRadius:7,
+        height: 40,
+        width: 400,
+        margin: 12,
+        borderWidth: 0.5,
+        padding: 5,
+        position: 'absolute',
+        right: 50,
+        bottom: 380,
+      },
+       inputE: {
+        borderRadius:7,
         height: 40,
         width: 400,
         margin: 12,
@@ -137,7 +280,7 @@ const styles = StyleSheet.create({
         minWidth: '50%',
         textAlign: 'center',
         position: 'relative',
-        bottom: 7,
+        bottom: 0,
        // right: 50,
   },
   button: {
