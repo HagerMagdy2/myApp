5ac8f6b5055e9a3b29e07c55ba3a30eb91eea26c
Arwa Ayman <109555559+arwaayman16@users.noreply.github.com>
2023-04-17 20:25:47 +0200

    Update Profile.js

--- Pages/Profile.js
+++ Pages/Profile.js
@@ -1,8 +1,26 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-
+import { doc, getDoc } from "firebase/firestore";
+import { auth,db } from "firebase/firestore";
 export default function Profile  ({navigation})  {
+  const[name , setName]=useState('');
+  const[email , setEmail]=useState('');
+  const[password , setPassword]=useState('');
+  const getUser =async()=>{
+    const docRef = doc(db, "users", auth.currentUser.uid);
+    const docSnap = await getDoc(docRef);
+    
+    if (docSnap.exists()) {
+      console.log("Document data:", docSnap.data());
+    } else {
+      // docSnap.data() will be undefined in this case
+      console.log("No such document!");
+    }
+
+    
+  }
   return (
+
     <View>
       <Text>Profile</Text>
     </View>
